e7bade708a8a40214e6110175ef316bcde147bae
Ryan Iug <ryaniugramos@gmail.com>
2023-05-16 22:33:28 -0300

    responsive components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { CardComponent } from './components/card/card.component';
@@ -10,6 +11,12 @@ import { ViewMoreBtnComponent } from './components/view-more-btn/view-more-btn.c
 import { FooterComponent } from './components/footer/footer.component';
 import { BurgerButtonComponent } from './components/burger-button/burger-button.component';
 import { HomeComponent } from './pages/home/home.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -22,7 +29,14 @@ import { HomeComponent } from './pages/home/home.component';
     BurgerButtonComponent,
     HomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'cinehub-bf4f4',
+    appId: '1:444137153722:web:443c89224a8cd30a258e83',
+    databaseURL: 'https://cinehub-bf4f4-default-rtdb.firebaseio.com',
+    storageBucket: 'cinehub-bf4f4.appspot.com',
+    apiKey: 'AIzaSyDrPdqjxfkdx0BGXES_iJiNpuNMoRHRfU4',
+    authDomain: 'cinehub-bf4f4.firebaseapp.com',
+    messagingSenderId: '444137153722',
+    measurementId: 'G-HYKH5V7GPB',
+  },
+};
